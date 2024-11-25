3d3d2b6afe7600d8eda9a9424d73e06a6235deaf
MG-esiee <mateo.gallina@edu.esiee.fr>
2024-11-25 12:22:40 +0100

    Update room.py

--- room.py
+++ room.py
@@ -1,7 +1,21 @@
 # Define the Room class.
 
 class Room:
+    """
+    La class des differentes Room (salles) qui sont dans le jeu
 
+    Attributs: 
+        name (str) : Le nom de la piece
+        items (str) : Les objets de la piece
+        descritption (str) : La description de la piece
+        exits (dict) : Les differentes sorties de la piece
+
+    Methodes:
+        get_exit : voir si la direction demandee par le joueur existe ou non
+        get_exit_string : renvoyer la description des sorties
+        get_long_description : renvoyer une longue description de la sortie
+        __init__ : constructeur de la classe
+    """
     # Define the constructor. 
     def __init__(self, name, description):
         self.name = name
